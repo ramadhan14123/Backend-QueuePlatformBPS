a20b9c5cb61f45e4161dba471dfad14509bb5d88
ramadhan14123 <ramadhangithub@gmail.com>
2025-08-16 20:41:24 +0700

    fix: add cors and fix from production

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from app import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app, resources={r"/*": {"origins": ["bukutamuapi.pahlawan140.com"]}})
+CORS(app, resources={r"/*": {"origins": ["https://bukutamu.pahlawan140.com"]}})
 
 if __name__ == "__main__":
     app.run(debug=True) #development
